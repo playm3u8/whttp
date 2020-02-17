2cfde1aa9d414fd532070e35fdaadbc338c78b8f
yang sifa <admin@playm3u8.com>
2020-02-17 19:38:54 +0800

    update

--- demo/index.php
+++ demo/index.php
@@ -2,23 +2,22 @@
 // ini_set("html_errors","On");
 // ini_set("display_errors","On");
 require_once dirname(__DIR__)."/autoload.php";
+use PL\Whttp;
 
-
+/*
 $http = get('http://o96.cc:4000/get.php', ['abc'=>'123','eee'=>'34555']);
 if ($http->getError()) {
 	$http = "error: ".$http->getError();
 } else {
-	$http = $http->getBody();
+	$http = $http->getAll();
 }
 p($http, true);
+*/
 
-
-/*
-$http = get('https://www.baidu.com')->timeout(200)->nobody(true);
+$http = Whttp::get('https://www.baidu.com')->timeout(200)->nobody(true);
 if ($http->getError()) {
 	$http = "error: ".$http->getError();
 } else {
-	$http = $http->getHeaders();
+	$http = $http->getAll();
 }
 p($http,true);
-*/
